f9365c3de852b68c7db61ae73d98af0feaff9c95
Jitendra Yashwantrao <jitendra@Jitendras-MacBook-Pro.local>
2021-08-16 08:55:57 +0530

    added two sum

--- main.go
+++ main.go
@@ -13,5 +13,31 @@ func main() {
 	uuid := strings.Replace(uuidWithHyphen.String(), "-", "", -1)
 	fmt.Println(uuidWithHyphen, "  ", uuid)
 	fmt.Println(welcome.Welcome())
+	inputNumbers := []int{12, 4, 5, 6, 1, 7}
+	fmt.Println(twoSum(inputNumbers, 7))
 
+}
+
+func twoSum(nums []int, target int) []int {
+
+	var answers []int
+	numbersMap := make(map[int]int)
+	for j := 0; j < len(nums); j++ {
+		numbersMap[nums[j]] = j
+
+	}
+
+	for j := 0; j < len(nums); j++ {
+		nextMatch := target - nums[j]
+
+		value, present := numbersMap[nextMatch]
+
+		if present && value != j {
+			answers = append(answers, j, value)
+			break
+		}
+
+	}
+
+	return answers
 }
